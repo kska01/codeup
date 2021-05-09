04acf8c0e639b4a98e830ed3f184a2ecf72f1a9a
ka <insideka01@gmail.com>
2021-05-09 11:31:45 +0900

    정수 3개 입력받아 합과 평균 출력

---
+++ basic_100/b1046.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	long long int a, b, c;
+	scanf("%lld %lld %lld", &a, &b, &c);
+	printf("%lld\n", a + b + c);
+	printf("%.1lf\n", (double)(a + b + c)/3);
+	return 0;
+}
+
