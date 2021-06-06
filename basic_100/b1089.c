10664f93a1d1d6c3c4169ffc74c1c0dc56068494
ka <insideka01@gmail.com>
2021-06-06 16:58:05 +0900

    수 나열하기1

---
+++ basic_100/b1089.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, d, n;
+
+    scanf("%d %d %d", &a, &d, &n);
+    for(int i = 1; i < n; i++)
+    {
+        a = a + d;
+    }
+    printf("%d\n", a);
+    return 0;
+}
