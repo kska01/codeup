0342e67ea33131705a6e0daf10a719684bd22a95
ka <insideka01@gmail.com>
2021-06-28 09:26:16 +0900

    이상한 출석 번호 부르기2

---
+++ basic_100/b1094.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i;
+    int k[10000];
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &k[i]);
+    }
+    for(i = n- 1; i >= 0; i--)
+    {
+        printf("%d ", k[i]);
+    }
+    return 0;
+}
